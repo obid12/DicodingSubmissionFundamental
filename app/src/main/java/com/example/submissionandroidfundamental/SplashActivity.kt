3111607093d2287bc6b68e0bdea505d1c76c081d
L Gilang Obidia Ramdhani <obidiaramdhani23@gmail.com>
2022-12-07 15:28:03 +0700

    android fundamental

---
+++ app/src/main/java/com/example/submissionandroidfundamental/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.submissionandroidfundamental
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+}
